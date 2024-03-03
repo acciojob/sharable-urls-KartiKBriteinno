82951e2f1574c9d8c5e62151856a9ff3407d0064
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-03-03 11:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+function updateURL() {
+        const name = document.getElementById('name').value;
+        const year = document.getElementById('year').value;
+        const baseURL = 'https://localhost:8080/';
+        const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
+        document.getElementById('url').textContent = baseURL + queryString;
+    }
