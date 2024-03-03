a9b4cf03bd3dae7b41b296fa2745aeba4d4f8910
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-03-03 11:59:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
    function updateURL() {
-	   cy.get('#button', { timeout: 10000 }).click();
         const name = document.getElementById('name').value;
         const year = document.getElementById('year').value;
         const baseURL = 'https://localhost:8080/';
         const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
         document.getElementById('url').textContent = baseURL + queryString;
+        return false; // Prevent form submission
     }
