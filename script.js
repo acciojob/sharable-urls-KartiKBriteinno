2a8b5db70ed51d8da461c743ac22e6db878de724
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-03-03 12:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
 app.post('/add', (req, res) => {
