bfe3e275de97575631710a88ac6feb0c42c8c241
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-03-03 12:01:45 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-   function updateURL() {
-        const name = document.getElementById('name').value;
-        const year = document.getElementById('year').value;
-        const baseURL = 'https://localhost:8080/';
-        const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
-        document.getElementById('url').textContent = baseURL + queryString;
-        return false; // Prevent form submission
-    }
